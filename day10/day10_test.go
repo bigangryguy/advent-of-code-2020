ccc6d9b3d3f234e3e7498a2489ffadda772c9b2f
David Wilcox <david@dtwil.co>
2020-12-10 07:50:41 -0600

    Forgot a day 10 test case

--- day10/day10_test.go
+++ day10/day10_test.go
@@ -25,3 +25,25 @@ func Test_part1(t *testing.T) {
 		t.Errorf("part1 = %d, expected = %d\n", actual, expected)
 	}
 }
+
+func Test_part2(t *testing.T) {
+	nums, err := getInput("day10_test_input.txt")
+	if err != nil {
+		t.Error("Received unexpected error")
+	}
+	expected := 8
+	actual := part2(nums)
+	if actual != expected {
+		t.Errorf("part2 = %d, expected = %d\n", actual, expected)
+	}
+
+	nums, err = getInput("day10_test_input2.txt")
+	if err != nil {
+		t.Error("Received unexpected error")
+	}
+	expected = 19208
+	actual = part2(nums)
+	if actual != expected {
+		t.Errorf("part2 = %d, expected = %d\n", actual, expected)
+	}
+}
