42a6969d494d829b8c3bd32d71d7f357119030a4
David Wilcox <david@dtwil.co>
2020-12-17 03:46:42 -0600

    Completed day 14 part 2

--- day14/day14.go
+++ day14/day14.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -18,10 +19,6 @@ func getInput(filename string) ([]string, error) {
 }
 
 func generateMasks(bitmask string) (orMask int64, andMask int64) {
-	if len(bitmask) != 36 {
-		return
-	}
-
 	for i, bit := range bitmask {
 		var val int64 = 1 << (35 - i)
 		if bit == '1' {
@@ -34,8 +31,53 @@ func generateMasks(bitmask string) (orMask int64, andMask int64) {
 	return
 }
 
+func combinations(values []int64) (output []int64) {
+	for i := 1; i < 1 << len(values); i++ {
+		var value int64
+		for j := 0; j < len(values); j++ {
+			if (i >> j) & 1 == 1 {
+				value += values[j]
+			}
+		}
+		output = append(output, value)
+	}
+	output = append(output, 0)
+	sort.Slice(output, func(i, j int) bool { return output[i] < output[j] })
+	return
+}
+
+func floatingBitsFromBitmask(bitmask string) (set []int64) {
+	for i, bit := range bitmask {
+		if bit == 'X' {
+			set = append(set, 1 << (35 - i))
+		}
+	}
+	set = combinations(set)
+	return
+}
+
+func fixedBitsFromBitmask(bitmask string) (result int64) {
+	for i, bit := range bitmask {
+		if bit == '1' {
+			result += 1 << (35 - i)
+		}
+	}
+	return
+}
+
 func applyMasks(value int64, orMask int64, andMask int64) int64 {
 	return (value | orMask) & andMask
+}
+
+func applyFloatingMask(value int64, bitmask string) (result []int64) {
+	fixedMask := fixedBitsFromBitmask(bitmask)
+	floatingMasks := floatingBitsFromBitmask(bitmask)
+	base := value | fixedMask
+	for _, mask := range floatingMasks {
+		result = append(result, base ^ mask)
+	}
+	sort.Slice(result, func(i, j int) bool { return result[i] < result[j] })
+	return
 }
 
 func part1(lines []string) (result int64, err error) {
@@ -61,8 +103,38 @@ func part1(lines []string) (result int64, err error) {
 			memory[memloc] = value
 		}
 	}
-	for _, values := range memory {
-		result += values
+	for _, value := range memory {
+		result += value
+	}
+	return
+}
+
+func part2(lines []string) (result int64, err error) {
+	memory := make(map[int64]int64)
+	var bitmask string
+	for _, line := range lines {
+		tokens := strings.Split(line, " = ")
+		if tokens[0] == "mask" {
+			bitmask = tokens[1]
+		} else {
+			var originalMemloc int64
+			originalMemloc, err = strconv.ParseInt(tokens[0][4:len(tokens[0])-1], 10, 64)
+			if err != nil {
+				return
+			}
+			var value int64
+			value, err = strconv.ParseInt(tokens[1], 10, 64)
+			if err != nil {
+				return
+			}
+			memlocs := applyFloatingMask(originalMemloc, bitmask)
+			for _, memloc := range memlocs {
+				memory[memloc] = value
+			}
+		}
+	}
+	for _, value := range memory {
+		result += value
 	}
 	return
 }
@@ -77,6 +149,11 @@ func main() {
 	if err != nil {
 		fmt.Printf("Received unexpected error: %v\n", err)
 	}
-
 	fmt.Printf("Part 1 answer: %d\n", part1Result)
+
+	part2Result, err := part2(lines)
+	if err != nil {
+		fmt.Printf("Received unexpected error: %v\n", err)
+	}
+	fmt.Printf("Part 2 answer: %d\n", part2Result)
 }
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -17,6 +17,30 @@ func Test_generateMasks(t *testing.T) {
 	}
 }
 
+func Test_combinations(t *testing.T) {
+	valuesList := [2][]int64 {
+		{ 32, 1 },
+		{ 8, 2, 1 },
+	}
+	expected := [2][]int64 {
+		{ 0, 1, 32, 33 },
+		{ 0, 1, 2, 3, 8, 9, 10, 11 },
+	}
+	for i, values := range valuesList {
+		actual := combinations(values)
+		if len(actual) != len(expected[i]) {
+			t.Errorf("Length of actual does not match length of expected\nactual = %v, expected = %v\n",
+				actual, expected[i])
+		}
+		for j, actualValue := range actual {
+			if actualValue != expected[i][j] {
+				t.Errorf("combinations: actual = %d at index %d, expected %d\n",
+					actualValue, j, expected[i][j])
+			}
+		}
+	}
+}
+
 func Test_applyMasks(t *testing.T) {
 	var orMask int64 = 64
 	var andMask int64 = 68719476733
@@ -29,6 +53,56 @@ func Test_applyMasks(t *testing.T) {
 		actual := applyMasks(testCase[0], orMask, andMask)
 		if actual != testCase[1] {
 			t.Errorf("applyMasks = %d for %d, expected %d", actual, testCase[0], testCase[1])
+		}
+	}
+}
+
+func Test_floatingBitsFromBitmask(t *testing.T) {
+	bitmask := "000000000000000000000000000000X1001X"
+	expected := []int64 { 0, 1, 32, 33 }
+	actual := floatingBitsFromBitmask(bitmask)
+	if len(actual) != len(expected) {
+		t.Fatalf("Length mismatch in floatingBitsFromBitmask. actual = %v, expected = %v\n",
+			actual, expected)
+	}
+	for i, actualValue := range actual {
+		if actualValue != expected[i] {
+			t.Errorf("floatingBitsFromBitmask = %d at index %d, expected %d\n",
+				actualValue, i, expected[i])
+		}
+	}
+}
+
+func Test_fixedBitsFromBitmask(t *testing.T) {
+	bitmask := "000000000000000000000000000000X1001X"
+	var expected int64 = 18
+	actual := fixedBitsFromBitmask(bitmask)
+	if actual != expected {
+		t.Errorf("fixedBitsFromBitmask = %d, expected %d\n", actual, expected)
+	}
+}
+
+func Test_applyFloatingMask(t *testing.T) {
+	bitmasks := []string {
+		"000000000000000000000000000000X1001X",
+		"00000000000000000000000000000000X0XX",
+	}
+	inputs := []int64 { 42, 26 }
+	expected := [2][]int64 {
+		{ 26, 27, 58, 59 },
+		{ 16, 17, 18, 19, 24, 25, 26, 27 },
+	}
+	for i := 0; i < 2; i++ {
+		actual := applyFloatingMask(inputs[i], bitmasks[i])
+		if len(actual) != len(expected[i]) {
+			t.Fatalf("Length mismatch in applyFloatingMask. actual = %v, expected = %v\n",
+				actual, expected[i])
+		}
+		for j, actualValue := range actual {
+			if actualValue != expected[i][j] {
+				t.Errorf("applyFloatingMask = %d at index %d, expected %d\n",
+					actualValue, j, expected[i][j])
+			}
 		}
 	}
 }
@@ -46,5 +120,21 @@ func Test_part1(t *testing.T) {
 	}
 	if actual != expected {
 		t.Errorf("part1 = %d, expected %d\n", actual, expected)
+	}
+}
+
+func Test_part2(t *testing.T) {
+	lines, err := getInput("day14_test_input2.txt")
+	if err != nil {
+		t.Errorf("Error getting input: %v\n", err)
+	}
+
+	var expected int64 = 208
+	actual, err := part2(lines)
+	if err != nil {
+		t.Errorf("Received unexpected error: %v\n", err)
+	}
+	if actual != expected {
+		t.Errorf("part2 = %d, expected %d\n", actual, expected)
 	}
 }
